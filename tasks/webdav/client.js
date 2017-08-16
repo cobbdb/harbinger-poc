7fc1794762ce2e59adcffb00673bf5141e7c355c
Dan Cobb <cobbdb@gmail.com>
2017-08-16 14:43:50 -0700

    .

---
+++ tasks/webdav/client.js
@@ -0,0 +1,12 @@
+var createClient = require('webdav'),
+    auth = require('../../auth.json'),
+    hosts = require('./hosts.json'),
+    cache = {};
+
+/**
+ * @param {string} env Can be one of `dev`, `qa`, `staging`, `prod`, `training`.
+ */
+module.exports = function (env) {
+    cache[env] = cache[env] || createClient(hosts[env], auth.username, auth.password);
+    return cache[env];
+};
--- tasks/write.js
+++ tasks/write.js
@@ -1,44 +1,5 @@
-var fs = require('fs'),
-    glob = require('glob'),
-    hosts = require('./http/hosts.json'),
-    writeFile = require('./util/write-file.js');
+var webdav = require('./webdav/client.js');
 
-module.exports = function (grunt) {
-    grunt.registerTask('write', function (type, option) {
-        var done = this.async();
-
-        if (type === 'methode') {
-            glob('**/*', {
-                cwd: 'src/methode',
-                nodir: true
-            }, function (err, files) {
-                if (err) {
-                    console.error(err);
-                    grunt.fail.fatal('Error in glob src/methode/**/*. Exiting without changes.');
-                    done();
-                } else {
-                    var count = files.length;
-                    function checkComplete() {
-                        count -= 1;
-                        if (count === 0) {
-                            done();
-                        }
-                    }
-
-                    files.forEach(function (path) {
-                        path = '/' + path;
-                        writeFile(grunt, {
-                            host: hosts.dev.address,
-                            path: path,
-                            data: grunt.file.read('src/methode' + path),
-                            success: checkComplete,
-                            error: checkComplete
-                        });
-                    });
-                }
-            });
-        } else {
-            grunt.fail.fatal('Unknown build type. Exiting without changes.');
-        }
-    });
+module.exports = function (path, env) {
+    var fout = webdav(env).createWriteStream(path);
 };
